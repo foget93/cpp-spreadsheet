a693b207b0cb57e46fbe61d8276f2a75404e8e21
Dmitrii Makarov <dmitriymakarov93@gmail.com>
2023-04-04 16:03:00 +0300

    add design

--- design/sheet.h
+++ design/sheet.h
@@ -4,11 +4,21 @@
 #include "common.h"
 
 #include <functional>
+#include <unordered_map>
+
+struct PositionHasher {
+    size_t operator()(Position pos) const;
+
+private:
+    std::hash<int> pos_hasher_;
+};
 
 class Sheet : public SheetInterface {
+    friend class Cell;
 public:
-    ~Sheet();
+    using Table = std::unordered_map<Position, std::unique_ptr<Cell>, PositionHasher>;
 
+public:
     void SetCell(Position pos, std::string text) override;
 
     const CellInterface* GetCell(Position pos) const override;
@@ -21,14 +31,11 @@ public:
     void PrintValues(std::ostream& output) const override;
     void PrintTexts(std::ostream& output) const override;
 
-    const Cell* GetConcreteCell(Position pos) const;
-    Cell* GetConcreteCell(Position pos);
-
 private:
-    void MaybeIncreaseSizeToIncludePosition(Position pos);
-    void PrintCells(std::ostream& output,
-                    const std::function<void(const CellInterface&)>& printCell) const;
-    Size GetActualSize() const;
 
-    std::vector<std::vector<std::unique_ptr<Cell>>> cells_;
+    void Resize(Position pos);
+    void CalculateNewSize();
+
+    Table table_;
+    Size size_;
 };
